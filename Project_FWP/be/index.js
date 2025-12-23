d6b0fdd655d47892a89aa30f042c59e5c4963007
owen550 <omoses611@gmail.com>
2025-12-23 13:25:15 +0700

    node module

--- Project_FWP/be/index.js
+++ Project_FWP/be/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
 const bahanBakuRoutes = require("./routes/bahanBakuRoutes");
 const login = require("./routes/loginRoutes");
 const menuManagement = require("./routes/menuManagement");
---
+++ Project_FWP/be/seeder.js
@@ -0,0 +1,10 @@
+const bahanBakuRoutes = require("./routes/bahanBakuRoutes");
+const login = require("./routes/loginRoutes");
+const menuManagement = require("./routes/menuManagement");
+const detailMenuRoutes = require("./routes/detailMenuRoutes");
+const pesananDetailRoutes = require("./routes/pesananDetailRoutes");
+const detailPenjualanRoutes = require("./routes/detailPenjualanRoutes");
+const mainPenjualanRoutes = require("./routes/mainPenjualanRoutes");
+const laporanKeuanganRoutes = require("./routes/laporanKeuanganRoutes");
+const historyRoutes = require("./routes/historyRoutes");
+
